d0f0eca15fadc67d065bafeeee9234f763a6c55e
= <=>
2018-04-11 18:30:37 +0700

    11/4

--- AutoRes.sh
+++ AutoRes.sh
@@ -3,6 +3,7 @@ echo "Hãy chắc chắn bạn đã tạo kho trên github.com"
 echo "Xin hãy chọn chức năng:"
 echo "- Nhập vào 1 để tạo kho mới tại thư mục hiện hành"
 echo "- Nhập vào 2 để tự động push lên github"
+echo "- Kiểm tra xem file đã được đẩy lên chưa? nếu thấy hiện màu đỏ các file hãy chọn tùy chọn 2"
 read -p "Nhập vào chức năng:" chucnang
 case $chucnang in 
 1)
@@ -11,9 +12,10 @@ case $chucnang in
 	git add README.md
 	read -p "Nhập vào tin nhắn của bạn để push lên github:" tinnhan
 	git commit -m "$tinnhan"
-	read -p "NHập vào đường dẫn kho github bạn đã tạo (copy link kho trên thanh địa chỉ +.git vào cuối link):" duongdan
-	git remote add origin $duongdan
-	git push -u origin master
+	read -p "NHập vào đường dẫn kho github bạn đã tạo (copy link kho trên thanh địa chỉ):" duongdan
+	git remote add origin $duongdan".git"
+	git config credential.helper store 
+	git push -f origin master
 	echo "Bạn đã tạo kho thành công!";;
 2)
 	git add .
@@ -21,6 +23,9 @@ case $chucnang in
 	git commit -m "$message"
 	git push -f origin master
 	echo "Đẩy file lên thành công";;
+3)
+	git status
+	echo "Hãy chạy lại chương trình";;
 *)
 	echo "Nhập sai chức năng!"
 esac 
